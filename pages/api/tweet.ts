2ae55fdf96744f9dcea8a08ba4e1ebc9f6901249
Alistair Vu <alistairvu@gmail.com>
2022-01-06 00:13:24 +0700

    Add tweet endpoint

--- pages/api/tweet.ts
+++ pages/api/tweet.ts
@@ -1,29 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
-import type { Result } from '../../utils';
-import { getResult } from '../../utils';
-import { TwitterApi } from 'twitter-api-v2';
+import { TwitterClient } from 'twitter-api-client';
+import type { Result } from '../../utils/lyric';
+import { getResult } from '../../utils/lyric';
 
-const twitterClient = new TwitterApi(
-  process.env.TWITTER_BEARER_TOKEN as string
-);
+const twitterClient = new TwitterClient({
+  apiKey: process.env.TWITTER_API_KEY as string,
+  apiSecret: process.env.TWITTER_API_SECRET as string,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
 
 const getLyric = async (res: NextApiResponse<Result>) => {
   const result = await getResult();
   return res.status(200).send(result);
 };
 
-const postLyric = async (res: NextApiResponse) => {
+const postLyric = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      throw new Error(`Invalid authorization header`);
+    }
+
+    const auth = authHeader.split(' ')[1];
+    const [user, password] = Buffer.from(auth, 'base64').toString().split(':');
+
+    if (
+      user !== process.env.AUTH_USERNAME ||
+      password !== process.env.AUTH_PASSWORD
+    ) {
+      throw new Error(`Invalid authorization header`);
+    }
+
     const { lyric } = await getResult();
-    await twitterClient.v1.tweet(lyric);
+
+    await twitterClient.tweets.statusesUpdate({ status: lyric });
     res.status(200).send({ success: true, lyric });
-  } catch (_) {
-    res.status(500).send({ success: false });
+  } catch (err: any) {
+    res.status(500).send({ success: false, message: err.message });
   }
 };
 
 export default nc()
   .get((_, res) => getLyric(res as NextApiResponse<Result>))
-  .post((_, res) => postLyric(res as NextApiResponse));
+  .post((req, res) => {
+    postLyric(req as NextApiRequest, res as NextApiResponse);
+  });
