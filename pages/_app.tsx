7ed4e69bd9aabe0b05ae8d0e926ca04f0126a2aa
Alistair Vu <alistairvu@gmail.com>
2022-01-29 22:38:59 +0700

    Add mixpanel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import mixpanel from 'mixpanel-browser';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_ID || '', {
+    debug: process.env.NODE_ENV !== 'production',
+  });
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      mixpanel.track(url);
+      console.log(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
